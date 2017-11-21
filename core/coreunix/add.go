3dfbdbef1bf67d08853a2ebef8b46b3f1dfbd6f2
Łukasz Magiera <magik6k@gmail.com>
2017-11-21 19:11:12 +0100

    providers: integrate with MFS
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/coreunix/add.go
+++ core/coreunix/add.go
@@ -122,7 +122,7 @@ func (adder *Adder) mfsRoot() (*mfs.Root, error) {
 	}
 	rnode := unixfs.EmptyDirNode()
 	rnode.SetPrefix(adder.Prefix)
-	mr, err := mfs.NewRoot(adder.ctx, adder.dagService, rnode, nil)
+	mr, err := mfs.NewRoot(adder.ctx, adder.dagService, adder.provide, rnode, nil)
 	if err != nil {
 		return nil, err
 	}
